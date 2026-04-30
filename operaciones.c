a207abd5fcd22493426284e9bd13742c8b199b72
Martina Pecorelli <mpecorelli@frba.utn.edu.ar>
2026-04-30 17:28:53 -0300

    agrega comentario en multiplicar

--- operaciones.c
+++ operaciones.c
@@ -13,6 +13,7 @@ int restar(int a, int b) {
 }
 
 /* ── multiplicar — implementar en feature/mi-funcion ─────────────────────── */
+//multiplica los números, commiteada
 
 int multiplicar(int a, int b) {
     return a * b;
